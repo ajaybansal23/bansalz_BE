312773c7e77a4617ac53b365573a4169b32bc4d7
Ajay Bansal <r.rupali211@gmail.com>
2020-08-07 22:42:08 +1000

    cleanup

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const passport = require('passport');
 
-const checkAuthorization = (req, res, next) => {
+const checkAuthentication = (req, res, next) => {
     if (req.isAuthenticated()) {
         console.log("user is authenticated")
         next();
     } else {
         console.log("user is not authenticated")
-        res.status(403);
+        res.status(401);
         res.send({ message: "user is not authenticated" });
     }
 }
@@ -36,18 +36,19 @@ module.exports = (app) => {
         return res.redirect(`/secure/home`);
     });
 
-    app.get('/api/secure/ceremonies', checkAuthorization, (req, res) => {
+    app.get('/api/secure/ceremonies', checkAuthentication, (req, res) => {
         console.log("sending response to ----> UI");
         // req.app.set('user', res.req.user)
         res.send({ name: "Ajay" });
     });
 
-    app.get('/api/secure/logout', checkAuthorization, (req, res) => {
+    app.get('/api/secure/logout', checkAuthentication, (req, res) => {
         req.logout();
-        res.send({ name: "user is now logged out" });
+        res.status(200);
+        res.send();
     });
 
-    app.get('/api/secure/authentication', checkAuthorization, (req, res) => {
+    app.get('/api/secure/authentication', checkAuthentication, (req, res) => {
         console.log("authentication is success. Now returning user object")
         res.status(200);
         res.send({ user: req.user });
